c4509582257c6eb23c0089eda91fb7e12916053d
HuVddme <valentine.ezikeoha@bison.howard.edu>
2026-07-17 16:21:59 -0400

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
@@ -18,6 +19,7 @@ export default function App() {
         <Contact />
       </main>
       <Footer />
+      <Analytics />
     </>
   );
 }
